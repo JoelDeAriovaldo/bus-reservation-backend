9d6f0decf9f9c6c4b309e62dcc779d37d72bf4ec
JoelDeAriovaldo <joeldeariovaldodevef@gmail.com>
2024-09-13 23:59:08 +0200

    Add migrations for creating User, Bus, Seat, Reservation, and Payment tables

---
+++ migrations/20240913214312-create-bus.js
@@ -0,0 +1,37 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Buses', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      number: {
+        type: Sequelize.STRING
+      },
+      type: {
+        type: Sequelize.STRING
+      },
+      capacity: {
+        type: Sequelize.INTEGER
+      },
+      availableSeats: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Buses');
+  }
+};
---
+++ migrations/20240913214403-create-reservation.js
@@ -0,0 +1,43 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Reservations', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      userId: {
+        type: Sequelize.INTEGER
+      },
+      busId: {
+        type: Sequelize.INTEGER
+      },
+      seatId: {
+        type: Sequelize.INTEGER
+      },
+      boardingPoint: {
+        type: Sequelize.STRING
+      },
+      dropOffPoint: {
+        type: Sequelize.STRING
+      },
+      journeyDate: {
+        type: Sequelize.DATE
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Reservations');
+  }
+};
